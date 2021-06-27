8a45bc90664561b6a2658925d20c81630c2c5cf3
MohammedFurkhan1 <86548017+MohammedFurkhan1@users.noreply.github.com>
2021-06-27 12:47:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import List from "./components/List";
+
+ReactDOM.render(
+  <div>
+    <h1 className="hello">Hello</h1>
+    <List />
+  </div>,
+  document.getElementById("root")
+);
